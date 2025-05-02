a7f837733aa5294e7122ce2c7d756dbaaac14471
tshims <bentshims21@gmail.com>
2025-05-03 00:04:23 +0200

    preconfiguration de tailwindcss

--- learnerReact/src/App.tsx
+++ learnerReact/src/App.tsx
@@ -1,15 +1,14 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import Header from './components/footer'
 import './App.css'
-
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <button className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-300">
-        Bouton Tailwind Test
-      </button>
-    </div>
-  );
+    <>
+      <Header />
+    </>
+  )
 }
+
 export default App
